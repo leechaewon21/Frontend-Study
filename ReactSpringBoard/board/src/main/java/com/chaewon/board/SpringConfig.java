2fcbc4064a378bb3f00282b6222a24061f026703
leechaewon21 <codnjs991122@gmail.com>
2022-07-05 19:38:41 +0900

    Update storing member's data method with h2 DB

--- ReactSpringBoard/board/src/main/java/com/chaewon/board/SpringConfig.java
+++ ReactSpringBoard/board/src/main/java/com/chaewon/board/SpringConfig.java
@@ -1,13 +1,26 @@
 package com.chaewon.board;
 
+import com.chaewon.board.repository.JDBCMemberRepository;
 import com.chaewon.board.repository.MemberRepository;
 import com.chaewon.board.repository.MemoryMemberRepository;
+import com.chaewon.board.service.APIService;
+import com.chaewon.board.service.BookService;
 import com.chaewon.board.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,7 +28,16 @@ public class SpringConfig {
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository() { return new MemoryMemberRepository(); }
+//    public MemberRepository memberRepository() { return new JDBCMemberRepository(dataSource); }
+
+
+    @Bean
+    public BookService bookService() { return new BookService(apiService());}
+
+    @Bean
+    public APIService apiService() {
+        return new APIService();
     }
+
 }
